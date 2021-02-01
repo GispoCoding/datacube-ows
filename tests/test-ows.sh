b8c54124d9a76d536d8ec4654cac5ed66facc32e
Mikael Vaaltola <mvaaltola@gmail.com>
2021-02-01 16:30:16 +0200

    Setup views before updating ranges

--- tests/test-ows.sh
+++ tests/test-ows.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 wait-for-db
-python /code/update_ranges.py
 datacube-ows-update --schema --role opendatacube
+python /code/update_ranges.py
 datacube-ows-update --views
 datacube-ows-update
 flask run --host=0.0.0.0 --port=8000
